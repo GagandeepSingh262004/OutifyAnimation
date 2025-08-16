29810ae64edc366af508f9e203ad5e5c2113fc97
Gagandeep Singh <gagandeepisheresince2004@gmail.com>
2025-08-16 19:21:56 +0530

    Previous Changes added

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ document.addEventListener("DOMContentLoaded", () => {
       opacity: 0.5,
       filter: "blur(5px)",
       scale: 1.05,
-      duration: 0.7,
+      duration: 1.2,
       ease: "power2.out",
     }
   )
-    .to(".image1", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
+    .to(".image1", { opacity: 0, duration: 1, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image2",
       { opacity: 0, scale: 0 },
       {
-        opacity: 0.5,
-        filter: "blur(3px)",
+        opacity: 0.7,
+        filter: "blur(5px)",
         scale: 1.1,
-        duration: 0.7,
+        duration: 1.2,
         ease: "power2.out",
       },
       "-=0.6"
     )
-    .to(".image2", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image2", { opacity: 0, duration: 0.8, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image3",
       { opacity: 0, scale: 0 },
-      { opacity: 1, scale: 1.15, duration: 0.7, ease: "power2.out" },
+      { opacity: 1, scale: 1.15, duration: 1.2, ease: "power2.out" },
       "-=0.6"
     )
-    .to(".image3", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6");
+    .to(".image3", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6");
   // .fromTo(
   //   ".image4",
   //   { opacity: 0, scale: 0.9 },
-  //   { opacity: 1, scale: 1.19, duration: 0.7, ease: "power2.out" },
+  //   { opacity: 1, scale: 1.19, duration: 1.2, ease: "power2.out" },
   //   "-=0.6"
   // )
-  // .to(".image4", { opacity: 0, duration: 0.5, ease: "power2.in" });
+  // .to(".image4", { opacity: 0, duration: 0.7, ease: "power2.in" });
 
   let t2 = gsap.timeline({ repeat: 0, paused: true });
   t2.fromTo(
@@ -48,38 +48,38 @@ document.addEventListener("DOMContentLoaded", () => {
       opacity: 0.5,
       filter: "blur(5px)",
       scale: 1.05,
-      duration: 0.7,
+      duration: 1.2,
       ease: "power2.out",
     }
   )
-    .to(".image5", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image5", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image6",
       { opacity: 0, scale: 0 },
       {
-        opacity: 0.5,
-        filter: "blur(3px)",
+        opacity: 0.7,
+        filter: "blur(5px)",
         scale: 1.1,
-        duration: 0.7,
+        duration: 1.2,
         ease: "power2.out",
       },
       "-=0.6"
     )
-    .to(".image6", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image6", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image7",
       { opacity: 0, scale: 0 },
-      { opacity: 1, scale: 1.15, duration: 0.7, ease: "power2.out" },
+      { opacity: 1, scale: 1.15, duration: 1.2, ease: "power2.out" },
       "-=0.6"
     )
-    .to(".image7", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6");
+    .to(".image7", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6");
   // .fromTo(
   //   ".image8",
   //   { opacity: 0, scale: 0.9 },
-  //   { opacity: 1, scale: 1.19, duration: 0.7, ease: "power2.out" },
+  //   { opacity: 1, scale: 1.19, duration: 1.2, ease: "power2.out" },
   //   "-=0.6"
   // )
-  // .to(".image8", { opacity: 0, duration: 0.5, ease: "power2.in" });
+  // .to(".image8", { opacity: 0, duration: 0.7, ease: "power2.in" });
 
   let t3 = gsap.timeline({ repeat: 0, paused: true });
   t3.fromTo(
@@ -89,38 +89,38 @@ document.addEventListener("DOMContentLoaded", () => {
       opacity: 0.5,
       filter: "blur(5px)",
       scale: 1.05,
-      duration: 0.7,
+      duration: 1.2,
       ease: "power2.out",
     }
   )
-    .to(".image9", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image9", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image10",
       { opacity: 0, scale: 0 },
       {
-        opacity: 0.5,
-        filter: "blur(3px)",
+        opacity: 0.7,
+        filter: "blur(5px)",
         scale: 1.1,
-        duration: 0.7,
+        duration: 1.2,
         ease: "power2.out",
       },
       "-=0.6"
     )
-    .to(".image10", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image10", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image11",
       { opacity: 0, scale: 0 },
-      { opacity: 1, scale: 1.15, duration: 0.7, ease: "power2.out" },
+      { opacity: 1, scale: 1.15, duration: 1.2, ease: "power2.out" },
       "-=0.6"
     )
-    .to(".image11", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6");
+    .to(".image11", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6");
   // .fromTo(
   //   ".image12",
   //   { opacity: 0, scale: 0.9 },
-  //   { opacity: 1, scale: 1.19, duration: 0.7, ease: "power2.out" },
+  //   { opacity: 1, scale: 1.19, duration: 1.2, ease: "power2.out" },
   //   "-=0.6"
   // )
-  // .to(".image12", { opacity: 0, duration: 0.5, ease: "power2.in" });
+  // .to(".image12", { opacity: 0, duration: 0.7, ease: "power2.in" });
 
   let t4 = gsap.timeline({ repeat: 0, paused: true });
   t4.fromTo(
@@ -130,43 +130,43 @@ document.addEventListener("DOMContentLoaded", () => {
       opacity: 0.5,
       filter: "blur(5px)",
       scale: 1.05,
-      duration: 0.7,
+      duration: 1.2,
       ease: "power2.out",
     }
   )
-    .to(".image13", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image13", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image14",
       { opacity: 0, scale: 0 },
       {
-        opacity: 0.5,
-        filter: "blur(3px)",
+        opacity: 0.7,
+        filter: "blur(5px)",
         scale: 1.1,
-        duration: 0.7,
+        duration: 1.2,
         ease: "power2.out",
       },
       "-=0.6"
     )
-    .to(".image14", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6")
+    .to(".image14", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6")
     .fromTo(
       ".image15",
       { opacity: 0, scale: 0 },
       {
         opacity: 1,
         scale: 1.15,
-        duration: 0.7,
+        duration: 1.2,
         ease: "power2.out",
       },
       "-=0.6"
     )
-    .to(".image15", { opacity: 0, duration: 0.5, ease: "power2.in" }, "-=0.6");
+    .to(".image15", { opacity: 0, duration: 0.7, ease: "power2.in" }, "-=0.6");
   // .fromTo(
   //   ".image16",
   //   { opacity: 0, scale: 0.9 },
-  //   { opacity: 1, scale: 1.19, duration: 0.7, ease: "power2.out" },
+  //   { opacity: 1, scale: 1.19, duration: 1.2, ease: "power2.out" },
   //   "-=0.6"
   // )
-  // .to(".image16", { opacity: 0, duration: 0.5, ease: "power2.in" });
+  // .to(".image16", { opacity: 0, duration: 0.7, ease: "power2.in" });
 
   let t5 = gsap.timeline({ paused: true });
   t5.fromTo(
